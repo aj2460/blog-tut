8a01173c7344b4ca3f17e43d8bff62dd042fbb73
Anand <ajkadakkal@gmail.com>
2019-03-05 20:33:50 +0530

    class base view for post on  home page (ListView)  and detail view

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
+
+
 # from django.http import HttpResponse
 
 
-
 def home(request):
-    context={
-        'posts':Post.objects.all()
+    context = {
+        'posts': Post.objects.all()
     }
-    return render(request,'blog/home.html',context)
+    return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html ->class based view template style
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
 
 def about(request):
-    return render(request, 'blog/about.html',{'title':'About'})
-
+    return render(request, 'blog/about.html', {'title': 'About'})
