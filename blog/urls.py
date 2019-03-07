0fc48e876db10378ff51711318c3741011b46d7e
Anand <ajkadakkal@gmail.com>
2019-03-07 22:18:08 +0530

    add pagination and filter by user

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 from .views import (PostListView, PostDetailView,
-                    PostCreateView, PostUpdateView, PostDeleteView)
+                    PostCreateView, PostUpdateView, PostDeleteView, UserPostListView)
 
 urlpatterns = [
     # path('', views.home, name='blog-home'),
     path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),  # view file is Post_form for create and update
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
